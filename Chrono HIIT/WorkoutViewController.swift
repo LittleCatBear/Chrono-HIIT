3b719f0eb6dbcca6ae7805860ac35a46da82b22f
LittleCatBear <j.huguet9@gmail.com>
2015-04-02 19:41:13 +1300

    workouts tbc

--- Chrono HIIT/WorkoutViewController.swift
+++ Chrono HIIT/WorkoutViewController.swift
@@ -10,3 +10,39 @@ import Foundation
 import UIKit
 import CoreData
 
+class WorkoutViewController:UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet weak var workoutTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        workoutTable.delegate = self
+        
+    }
+    
+    func getWorkouts(){
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return exercises.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.exerciseTableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        cell.textLabel?.text = exercises[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            exercises.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
+    }
+
+}
