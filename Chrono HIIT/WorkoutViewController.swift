a0cfc5fb833c34c6a119cce9e6f9bec5c198077a
LittleCatBear <j.huguet9@gmail.com>
2015-04-02 20:30:09 +1300

    workout

--- Chrono HIIT/WorkoutViewController.swift
+++ Chrono HIIT/WorkoutViewController.swift
@@ -13,34 +13,54 @@ import CoreData
 class WorkoutViewController:UIViewController, UITableViewDelegate, UITableViewDataSource{
     
     @IBOutlet weak var workoutTable: UITableView!
+    var workouts:[Workout] = [Workout]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         workoutTable.delegate = self
+        self.workoutTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        getWorkouts()
         
     }
     
     func getWorkouts(){
+        let fetchRequest = NSFetchRequest(entityName:"Workout")
+        
+        var error: NSError?
+        
+        let fetchedResults =
+        managedObjectContext?.executeFetchRequest(fetchRequest,
+            error: &error) as [Workout]?
+        
+        if let results = fetchedResults {
+            workouts = results as [Workout]
+            println(workouts[0].name)
+        } else {
+            println("Could not fetch \(error), \(error!.userInfo)")
+        }
         
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return exercises.count
+        return workouts.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell:UITableViewCell = self.exerciseTableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
-        cell.textLabel?.text = exercises[indexPath.row].name
+        var cell:UITableViewCell = self.workoutTable.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        println(workouts[indexPath.row].name)
+        cell.textLabel?.text = workouts[indexPath.row].name
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        //
+        workout = workouts[indexPath.row]
         
     }
     
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == UITableViewCellEditingStyle.Delete {
-            exercises.removeAtIndex(indexPath.row)
+            workouts.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
         }
     }
